3ac45812c4ee3f6b8b8c56afc874bda6f872734c
Jhonatan Queiroz <jhonatanqsilva@gmail.com>
2018-12-08 11:42:25 -0200

    Adição de rotas

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -10,15 +11,18 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
 
   model: any = {};
-  constructor(public authService: AuthService, private alertity: AlertifyService) { }
+  constructor(public authService: AuthService, private alertity: AlertifyService,private router: Router) { }
 
   ngOnInit() {
   }
   login() {
     this.authService.login(this.model).subscribe(next => {
       this.alertity.success('login sucesso');
+
     }, error => {
       this.alertity.error('falha  login');
+    }, () =>{
+      this.router.navigate(['/members']);
     });
   }
 
@@ -29,5 +33,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertity.message('logged out');
+    this.router.navigate(['/home']);
   }
 }
